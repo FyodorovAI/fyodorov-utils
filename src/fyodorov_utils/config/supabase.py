67680a25caa6fcd21a559efa34693c1fe02fdf37
Daniel Ransom <ransomdaniell@yahoo.com>
2024-02-06 19:26:03 +0100

    allow using access token (for rls)

--- src/fyodorov_utils/config/supabase.py
+++ src/fyodorov_utils/config/supabase.py
@@ -1,7 +1,17 @@
 from supabase import create_client, Client
+from supabase.lib.client_options import ClientOptions
 from .config import Settings
 
 settings = Settings()
 
-def get_supabase() -> Client:
-    return create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
+def get_supabase(jwt: str = None) -> Client:
+    if jwt:
+        return create_client(
+            settings.SUPABASE_URL,
+            settings.SUPABASE_KEY,
+            options=ClientOptions(
+                headers={"Authorization": f"Bearer {jwt}"}
+            )
+        )
+    else:
+        return create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
