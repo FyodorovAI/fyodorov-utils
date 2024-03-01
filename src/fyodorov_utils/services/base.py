6c3a24e72316df923df2325ca89036339154ac4e
Daniel Ransom <ransomdaniell@yahoo.com>
2024-03-01 02:50:14 +0100

    update

--- src/fyodorov_utils/services/base.py
+++ src/fyodorov_utils/services/base.py
@@ -3,28 +3,27 @@ from datetime import datetime, timedelta
 from fyodorov_utils.config.supabase import get_supabase
 from fyodorov_llm_agents.tools.tool import Tool as ToolModel
 
-class BaseModel():
+class Model():
     id: str = None
 
     def to_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
 
     def from_dict(self, data):
         for field in self.__table__.columns:
             if field.name in data:
                 setattr(self, field.name, data[field.name])
 
-class Base(BaseModel):
+class Base(Model):
     table: str = 'ERROR: table not set'
-    base_object: BaseModel = BaseModel()
 
     @staticmethod    
     def create_in_db(access_token: str) -> str:
         try:
             supabase = get_supabase(access_token)
-            result = supabase.table(self.table).insert(self.base_object.to_dict()).execute()
-            self.base_object.id = result.data[0]['id']
-            return self.base_object.id
+            result = supabase.table(self.table).insert(self.to_dict()).execute()
+            self.id = result.data[0]['id']
+            return self.id
         except Exception as e:
             print('Error creating object in table', self.table, str(e))
             raise e
@@ -34,8 +33,9 @@ class Base(BaseModel):
         if not id:
             raise ValueError('ID is required to update in table', self.table)
         try:
+            self.id = id
             supabase = get_supabase(access_token)
-            result = supabase.table(self.table).update(self.base_object).eq('id', id).execute()
+            result = supabase.table(self.table).update(self.to_dict()).eq('id', self.id).execute()
             return result.data[0]
         except Exception as e:
             print('An error occurred while updating table', self.table, id, str(e))
@@ -46,21 +46,23 @@ class Base(BaseModel):
         if not id:
             raise ValueError('ID is required to delete from table', self.table)
         try:
+            self.id = id
             supabase = get_supabase(access_token)
-            result = supabase.table(self.table).delete().eq('id', id).execute()
+            result = supabase.table(self.table).delete().eq('id', self.id).execute()
             return True
         except Exception as e:
             print('Error deleting from table', self.table, str(e))
             raise e
 
     @staticmethod
-    def get_in_db(access_token: str, id: str) -> BaseModel:
+    def get_in_db(access_token: str, id: str) -> Model:
         if not id:
             raise ValueError('ID is required to get from table', self.table)
         try:
+            self.id = id
             supabase = get_supabase(access_token)
-            result = supabase.table(self.table).select('*').eq('id', id).limit(1).execute()
-            self.base_object.from_dict(result.data[0])
+            result = supabase.table(self.table).select('*').eq('id', self.id).limit(1).execute()
+            self.from_dict(result.data[0])
             return self.base_object
         except Exception as e:
             print('Error fetching tool', str(e))
