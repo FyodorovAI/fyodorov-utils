a452a129edb937953958231b55b7c64c6242aca3
Daniel Ransom <dantheman0207@users.noreply.github.com>
2024-03-01 17:04:29 +0100

    changes

--- src/fyodorov_utils/templates/agents.py
+++ src/fyodorov_utils/templates/agents.py
@@ -2,12 +2,24 @@ from fyodorov_llm_agents.tools.tool import Tool as ToolModel
 from fyodorov_llm_agents.agents.agent import Agent as AgentModel
 import yaml
 
-def parse_yaml(yaml_str: str) -> ToolModel:
+def parse_yaml(yaml_str: str) -> dict:
     if not yaml_str:
         raise ValueError('YAML string is required')
     print(f"Parsing YAML: {yaml_str}")
     yaml_str = yaml_str.strip()
     yaml_dict = yaml.safe_load(yaml_str)
+    print(f"yaml_dict: {yaml_dict}")
+    if 'version' not in yaml_dict:
+        raise ValueError('fyodorov config version is required')
+    providers = []
+    for provider in yaml_dict['providers']:
+        print(f"Parsing provider: {provider}")
+        print(f"body: {yaml_dict['providers'][provider]}")
+        provider = ProviderModel(**yaml_dict['providers'][provider])
+        provider.validate()
+        providers.append(provider)
+    print(f"providers: {providers}")
+    models = []
     print(f"Parsing agents YAML in format: {yaml_dict['version']}")
     agents = []
     for agent in yaml_dict['agents']:
@@ -17,4 +29,6 @@ def parse_yaml(yaml_str: str) -> ToolModel:
         agent.validate()
         agents.append(agent)
     print(f"agents: {agents}")
-    return agents
+    return {
+        'agents': agents
+    }
